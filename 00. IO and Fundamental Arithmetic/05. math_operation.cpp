2c23029622e5a7553d4d5e91e30c6e1038366652
yuseon27 <ysnam19@naver.com>
2020-03-11 16:22:12 +0900

    math operation

---
+++ 00. IO and Fundamental Arithmetic/05. math_operation.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a = 0, b = 0;
+
+	cin >> a >> b;
+
+	cout << a + b << endl;
+	cout << a - b << endl;
+	cout << a * b << endl;
+
+	// cout.precision(15);
+	// cout << a / b << endl;
+	cout << a / b << endl;
+
+	cout << a % b << endl;
+
+
+	return 0;
+}
