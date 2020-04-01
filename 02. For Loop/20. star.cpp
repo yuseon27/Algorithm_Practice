fe617a5264cad6622c33ed8b5ab1434314df5783
yuseon27 <ysnam19@naver.com>
2020-04-02 04:47:35 +0900

    Star

---
+++ 02. For Loop/20. star.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int a = 0;
+	cin >> a;
+
+	for (int i = 0; i < a; i++) {
+		for (int j = 0; j <= i; j++) {
+			cout << "*";
+		}
+		cout << '\n';
+	}
+
+
+	return 0;
+}
